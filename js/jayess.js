d9f88811fa316774e343ed08729e6f2ec73153a2
organicbran <bryanchung987@gmail.com>
2023-07-17 01:43:27 -0700

    Alt text and mobile fix

--- js/jayess.js
+++ js/jayess.js
@@ -2,6 +2,7 @@ window.onscroll = function() {scrollFunction()};
 function scrollFunction() {
     let switchPoint = window.scrollY + document.getElementById("block-2").getBoundingClientRect().top - document.getElementById("bar").clientHeight;
     if (document.body.scrollTop > switchPoint || document.documentElement.scrollTop > switchPoint) {
+        document.body.style.backgroundColor = getComputedStyle(document.body).getPropertyValue('--black');
         document.getElementById("bar").classList.add("dark")
         document.getElementById("icon").classList.add("dark");
         var links = document.getElementsByClassName("bar-link");
@@ -9,6 +10,7 @@ function scrollFunction() {
             links[i].classList.add("dark");
         }
     } else {
+        document.body.style.backgroundColor = getComputedStyle(document.body).getPropertyValue('--yellow');
         document.getElementById("bar").classList.remove("dark")
         document.getElementById("icon").classList.remove("dark");
         var links = document.getElementsByClassName("bar-link");
